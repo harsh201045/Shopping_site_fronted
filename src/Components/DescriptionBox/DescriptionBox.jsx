c8279d96a71e224f70acb735562d709193ecf628
harsh201045 <harshpabani2010@gmail.com>
2024-07-19 10:31:36 +0530

    upload shopping site fronted

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (145)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita, corrupti est ex, excepturi illum aspernatur, voluptatem libero consequuntur sint voluptatibus non cumque magni dolores aliquid aliquam doloribus voluptate ipsum dignissimos dolore officia odio sunt quis laborum unde. Voluptas delectus rerum laborum accusantium! At, doloremque cupiditate!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi itaque, harum numquam pariatur voluptatem rerum! Sint quod dolorum possimus aperiam eaque! Placeat error obcaecati non.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
